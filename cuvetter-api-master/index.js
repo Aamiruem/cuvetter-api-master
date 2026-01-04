4feb32b96bbd4b4f7369ad2112ce2504bb43f8aa
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-01-04 19:29:53 +0530

    commit add

--- cuvetter-api-master/index.js
+++ cuvetter-api-master/index.js
@@ -33,3 +33,46 @@ app.use('/jobs', jobRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+// import dotenv from "dotenv";
+// import express from "express";
+// import mongoose from "mongoose";
+// import cors from "cors";
+
+// import authRoutes from "./routes/auth.js";
+// import jobRoutes from "./routes/jobs.js";
+
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// const corsOptions = {
+//     origin: "*",
+//     credentials: true,
+//     optionSuccessStatus: 200
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.json());
+
+// mongoose.connect(process.env.MONGODB_URI)
+//     .then(() => console.log("MongoDB connected"))
+//     .catch((err) => console.log("MongoDB connection error:", err));
+
+// app.get("/", (req, res) => {
+//     res.send("Test!");
+// });
+
+// app.use("/auth", authRoutes);
+// app.use("/jobs", jobRoutes);
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
